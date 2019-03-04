ec799320357d555afbddf1500513f291f743c803
Joonil Kim <somewehr@gmail.com>
2019-03-04 19:35:15 +0900

    fix unhealthy target issue

--- infra/init/secg.tf
+++ infra/init/secg.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "lb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
